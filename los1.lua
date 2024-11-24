62cca1e1fcc8d9335ca38799d6b87aac37e7f008
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-24 20:21:24 -0300

    los1.lua

---
+++ los1.lua
@@ -0,0 +1,9 @@
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/VyrosxC-Hub/IDK/refs/heads/main/ui.lua"))();
+
+local Notify = AkaliNotif.Notify;
+
+Notify({
+    Description = "  DESCRIPTION BLA BLA BLA";
+    Title = " TITLE BLA BLA BLA";
+    Duration = 5;
+    });
