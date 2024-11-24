cf4588c05fea1e50e141f7b0230576e9679e0814
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-24 20:21:59 -0300

    Delete los1.lua

--- los1.lua
+++
@@ -1,9 +0,0 @@
-local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/VyrosxC-Hub/IDK/refs/heads/main/ui.lua"))();
-
-local Notify = AkaliNotif.Notify;
-
-Notify({
-    Description = "  DESCRIPTION BLA BLA BLA";
-    Title = " TITLE BLA BLA BLA";
-    Duration = 5;
-    });
