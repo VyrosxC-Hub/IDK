42792280be93a45e71869dfdcce26b0d2eeb76fe
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-24 18:58:27 -0300

    los.lua

--- los.lua
+++ los.lua
@@ -1,41 +1,81 @@
-local DarkLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/ui-libs/main/Dark_Hub_Ui_Library/DarkHubUiLibrary"))() 
-local WindowOne = DarkLib:Window() -- Makes Window
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/VyrosxC-Hub/IDK/refs/heads/main/ui.lua"))()
 
-local TabOne = WindowOne:Tab("Tab One") -- Makes Tab
-local TabTwo = WindowOne:Tab("Tab Twp") -- Makes Tab
-local TabThree = WindowOne:Tab("Tab Three") -- Makes Tab
-local TabFour = WindowOne:Tab("Tab Four") -- Makes Tab
-local TabFive = WindowOne:Tab("Tab Five") -- Makes Tab
-local TabSix = WindowOne:Tab("Tab Siz") -- Makes Tab
-local TabSeven = WindowOne:Tab("Tab Siz") -- Makes Tab
-local TabEight = WindowOne:Tab("Tab Siz") -- Makes Tab
+local Window = library:AddWindow("VyroxC Hub", {
+		main_color = Color3.fromRGB(41, 74, 122),
+		min_size = Vector2.new(500, 600),
+		toggle_key = Enum.KeyCode.RightShift,
+		can_resize = true,
+	})
+	local Tab = Window:AddTab("Tab 1")
 
+	do -- Elements
+		Tab:AddLabel("Hello World!")
 
+		Tab:AddButton("Button", function()
+			print("Button clicked.")
+		end)
 
+		Tab:AddTextBox("TextBox", function(text)
+			print(text)
+		end, {
+			["clear"] = false, -- Default: true (options are optional)
+		})
 
+		local Switch = Tab:AddSwitch("Switch", function(bool)
+			print(bool)
+		end)
+		Switch:Set(true)
 
+		local Slider = Tab:AddSlider("Slider", function(x)
+			print(x)
+		end, { -- (options are optional)
+			["min"] = 0, -- Default: 0
+			["max"] = 100, -- Default: 100
+			["readonly"] = false, -- Default: false
+		})
+		Slider:Set(50)
 
-TabOne:Button("Button", function() -- Makes Button
-    --Button
-    end)
-    
-TabOne:Toggle("Toggle", function()
-    --Toggle
-    end)
-TabOne:Slider("Slider", 0, 50, function()
-    --Slider
-    end)
+		Tab:AddKeybind("Keybind", function(key)
+			print(key)
+		end, { -- (options are optional)
+			["standard"] = Enum.KeyCode.RightShift -- Default: RightShift
+		})
 
+		local Dropdown = Tab:AddDropdown("Dropdown", function(object)
+			print(object)
+		end)
+		for i = 1, 9 do
+			Dropdown:Add(tostring(i))
+		end
+		local obj = Dropdown:Add("10")
+		obj:Remove()
 
+		local CP = Tab:AddColorPicker(function(color)
+			print(color)
+		end)
+		CP:Set(Color3.new(1, 0, 0))
 
---TabOne:Colorpicker("Color Picker", preset, callback)
+		local Console = Tab:AddConsole({
+			["y"] = 100,
+			["source"] = "Lua",
+		})
+		Console:Set("-- Gamer time!\nfor i = 1, 9 do \n    print(i)\nend")
+		print(Console:Get())
 
-TabOne:Textbox("Text Box", "disapper", function()
-    --Text Box
-    end)
+		local HA = Tab:AddHorizontalAlignment()
+		HA:AddButton("Execute", function()
+			loadstring(Console:Get())()
+		end)
+		HA:AddButton("Clear", function()
+			Console:Set("")
+		end)
 
-TabOne:Label("Label")
-    
-TabOne:Dropdown("DropDown", list, callback)
+		local Folder = Tab:AddFolder("Folder") -- This can contain exactly the same as a Tab. You can have as many folders as you'd like to.
+		Folder:AddLabel("Hello")
+		local Folder2 = Folder:AddFolder("?")
+		Folder2:AddLabel("Woo!")
 
-TabOne:Keybind("KeyBind", key, callback)
+	end
+
+	Tab:Show()
+	library:FormatWindows()
