a1a7d2e0b472374b3e87d6d9066b9bb131a66539
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-24 21:42:49 -0300

    los.lua

--- los.lua
+++ los.lua
@@ -1,53 +1,32 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/VyrosxC-Hub/IDK/refs/heads/main/ui.lua", true))()
 
--- Criar uma janela principal
-local Window = Library:CreateWindow("Main Window")
+local AimbotTab = Library:CreateTab("Aimbot", "This is where you modify the Aimbot")
 
--- Adicionar uma pasta (opcional para organizar)
-local Folder = Window:AddFolder("Main")
+AimbotTab:CreateTextbox("Whitelist Player", function(arg) --arg is what the text is inside the textbox
+    print("Whitelisted: " .. arg)
+end)
 
--- Adicionar um rótulo (label)
-Folder:AddLabel({text = "Bem-vindo à Interface"})
+AimbotTab:CreateButton("Inf Jump", function() --you dont need "arg" for a button
+    print("Inf Jump is now permanently on.")
+end)
 
--- Adicionar um botão
-Folder:AddButton({
-    text = "Clique Aqui",
-    flag = "button",
-    callback = function()
-        print("Botão clicado!")
+
+AimbotTab:CreateSlider("Fov Radius", 0, 600, function(arg) --the (arg) is the sliders value
+    print("Fov Radius is set to:", arg)
+end)
+
+AimbotTab:CreateDropDown("Aimbot Part", {"Head", "Torso"}, function(arg) --the (arg) is the option you choose 
+    if arg == "Head" then
+        print("HEahshoot")
+    elseif arg == "Torso" then
+        print("trrrrso")
     end
-})
+end)
 
--- Adicionar um toggle
-Folder:AddToggle({
-    text = "Ativar Função",
-    flag = "toggle_function",
-    state = false,
-    callback = function(state)
-        print("Função está:", state and "Ativada" or "Desativada")
+AimbotTab:CreateToggle("Enable Aimbot", function(arg) --the (arg) is if the checkbox is toggled or not
+    if arg then
+        print("Aimbot is now : Enabled")
+    else
+        print("Aimbot is now : Disabled")
     end
-})
-
--- Adicionar um TextBox
-Folder:AddBox({
-    text = "Digite algo",
-    flag = "textbox_input",
-    value = "Texto padrão",
-    callback = function(input)
-        print("Texto digitado:", input)
-    end
-})
-
--- Adicionar uma lista suspensa (List)
-Folder:AddList({
-    text = "Selecione uma Opção",
-    flag = "option_list",
-    value = "Opção 1",
-    values = {"Opção 1", "Opção 2", "Opção 3"},
-    callback = function(selected)
-        print("Opção selecionada:", selected)
-    end
-})
-
--- Inicializar a biblioteca
-Library:Init()
+end)
